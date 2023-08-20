80212d848352772c8a7081097526434097fa716e
eugealbrecht <albrechteuge@gmail.com>
2023-08-19 21:48:27 -0300

    - Login y logout. - Login_requirement en 'información'

--- donarsangre/donarsangre/sitio/views.py
+++ donarsangre/donarsangre/sitio/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.decorators import login_required
+
 
 def inicio(request):
     return render(request, 'inicio.html', {})
+
+@login_required(login_url='login/')
+def informacion(request):
+    return render(request, 'informacion.html', {})
+
+def login(request):
+    return render(request, 'login.html', {})
